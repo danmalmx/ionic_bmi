d7d881c5c3e6cc937ee342e6701a8015cad11ceb
danmalmx <danmalmx@gmail.com>
2018-10-15 08:48:25 +0200

    Some changes to the acceptance test - issues still occurring

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,16 @@
 import { Calculator } from './app.po';
 
+
 describe('App', () => {
   let calulator: Calculator;
 
   beforeEach(() => {
-    calulator = new Calculator();
+    calulator = new Calculator();    
   });
 
   describe('default screen', () => {
     beforeEach(() => {
-        calulator.navigateTo('/');
+      calulator.navigateTo('/');
     });
 
     it('should have a title saying calculator', () => {
@@ -18,17 +19,26 @@ describe('App', () => {
       });
     });
 
-    it('should have an input field weight', () => {
+    it('should have an input field for weight', () => {
       calulator.weightInput().then(weight => {
       expect(weight).toEqual('Weight (kg)');
     });
   });
 
-  })
+    it('should have an input field for height', () => {
+      calulator.heightInput().then(height => {
+      expect(height).toEqual('Height (cm)');
+    });
+  });
 
     it('fill in form', () => {
-        calulator.fillInForm(70, 170);
-        expect(calulator.results_card_header()).toContain('BMI Calculations');
-        expect(calulator.results_card_content()).toContain('BMI: 24.22, You are Normal');
+      calulator.fillInForm(70, 170);
+      expect(calulator.results_card_header()).toContain('BMI Calculations');
+      expect(calulator.results_card_content()).toContain('BMI: 24.22, You are Normal');
     });
-});
+  });
+
+    it('should have a button saying Calculate', () => {
+      expect(calulator.getButtonText()).toContain('Calculate')
+    });
+  });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -2,6 +2,8 @@ import { browser, by, element } from 'protractor';
 
 export class Calculator {
 
+beforeEach() {
+}   
   navigateTo(destination) {
     return browser.get(destination);
   }
@@ -14,32 +16,30 @@ export class Calculator {
     return element(by.tagName('calculator')).element(by.tagName('ion-title')).getText();
   }
 
+  getButtonText() {
+    return element(by.tagName('page-calculator')).element(by.id('clc')).getText();
+  }
+
   weightInput() {
-    return element(by.id('lbl-0')).getText();  }
+    return element(by.id('kg')).getText();
+  }
 
   heightInput() {
-    return element(by.tagName('Height (cm)')).element(by.tagName('ion-label')).getText();
+    return element(by.id('cm')).getText();
   }
 
   fillInForm(weight, height) {
 
-    element(by.css('.text-input')).clear().then(() => {  // first we need to clear the default value
-        element(by.css('.text-input')).sendKeys(weight); // fill in the weight
+    element(by.css('.text-input')).clear().then(() => {
+        element(by.css('.text-input')).sendKeys(weight);
     }) 
 
-    element(by.css('.select')).click().then(() => {  // find the dropdown and click on it
-        browser.sleep(500); // we sleep for a half a second to make sure the popup has popped up
+    element(by.css('.text-input')).clear().then(() => {
+      element(by.css('.text-input')).sendKeys(height);
+  }) 
 
-        element(by.css('.text-input')).sendKeys(height); // click the height option you want
-        // element(by.cssContainingText('.button-inner', 'Calculate')).click();  // click the Calculate button
-
-    })
-
-    browser.sleep(500); // we sleep for half a second to make sure the popup has disappeared
-
-    element(by.cssContainingText('.button', 'Calculate')).click();  // click the calculate button
-
-}
+    element(by.cssContainingText('.button', 'Calculate')).click();
+  }
 
 results_card_header() {
     return element(by.css('ion-card-header')).getText();
